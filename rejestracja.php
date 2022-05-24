db4b4452a4593624b1a506458b44dcde94746b1c
Natalia <nasarzynska@gmail.com>
2022-05-24 14:59:48 +0200

    rejestracja update masa

--- rejestracja.php
+++ rejestracja.php
@@ -9,7 +9,7 @@
 
 
         <form method="POST" action ="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-            <input type = "text" name = "name" placeholder = "Imię i nazwisko"> <br>
+            <input type = "text" name = "name" placeholder = "Nazwa użytkownika"> <br>
             <input type = "email" name = "email" placeholder = "E-mail"> <br>
             <input type = "password" name = "password" placeholder = "Hasło"> <br>
             <input type = "text" name ="pregnancy_week" placeholder = "Tydzień ciąży"> <br>
@@ -92,21 +92,26 @@ $dbconn = mysqli_connect($servername, $username, $dbpassword, $dbname);
 
 
     $user_fullname_nowhitespaces = preg_replace('/\s+/', '', $user_fullname);
+
     $user_gl = $user_fullname_nowhitespaces. "_glikemia";
+    $user_masa = $user_fullname_nowhitespaces. "_masa";
 
     $user_password_hash = password_hash($user_password, PASSWORD_DEFAULT);
 
     if((isset($name) and isset($password)) and isset($email) and isset($pregweek)){
-        mysqli_query($dbconn, "INSERT INTO users (user_fullname, user_email, user_passwordhash, gl_pomiary, user_preg_week)
-         VALUES ('$user_fullname', '$user_email', '$user_password_hash', '$user_gl', '$user_preg_week')");
+
+        mysqli_query($dbconn, "INSERT INTO users (user_fullname, user_email, user_passwordhash, gl_pomiary, user_preg_week, masa_tabela)
+        VALUES ('$user_fullname', '$user_email', '$user_password_hash', '$user_gl', '$user_preg_week', '$user_masa')");
+        
+        mysqli_query($dbconn,"CREATE TABLE $user_gl (pomiar INT(100), data_pom DATE, time_pom TIME, meal VARCHAR(60))");
+        mysqli_query($dbconn,"CREATE TABLE $user_masa (data_masa DATE, pomiar_masa INT(100))");
+        
         echo "Rejestracja przebiegła poprawnie <br/><br/>";
 
     } else {
         echo " " .$imErr. "<br>" .$mailErr. "<br>" .$passErr. "<br>" .$freeimErr. "<br>".$freemailErr. "<br>" .$pregErr. "<br>";
     }
 
-    mysqli_query($dbconn,"CREATE TABLE $user_gl (pomiar INT(100), data_pom DATE, time_pom TIME, meal VARCHAR(60))");
-    
 ?>
 
 <br>
